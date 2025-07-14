9b27573d3630700cfe187a4f0a17bbf4b48b7181
Gayatri <arisamGayatri@gmail.com>
2025-07-14 12:24:38 +0530

    1st Commit from teammember

--- src/main/java/com/Banking/bankingsystem/controllers/DebitController.java
+++ src/main/java/com/Banking/bankingsystem/controllers/DebitController.java
@@ -1,8 +1,17 @@
 package com.Banking.bankingsystem.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DebitController {
 
+	@RequestMapping("/")
+	public String getData()
+	{
+		return "getData";
+	}
+	
+	
+	
 }
