a19fbdb3932596f4618b74a59e992c42f762650d
Gayatri <arisamGayatri@gmail.com>
2025-07-14 11:41:24 +0530

    1st Commit for project Banking system

---
+++ src/test/java/com/Banking/bankingsystem/BankingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Banking.bankingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
